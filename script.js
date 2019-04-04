7ef5d5e3fe883cdc3a80095a1dcc6111103f47aa
Tabywaby <43444241+Tabywaby@users.noreply.github.com>
2019-04-04 09:10:05 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$("button").click(function(){
+   var sponge=$(".sponge").val();
+   var food=$(".sponge").val();
+   if(sponge > 5 && food==="krusty krab"){
+       
+   } else if(sponge > 5 && food==="chum bucket"){
+       
+   } else if(sponge < 5 && food==="krusty krab"){
+       
+   } else if(sponge < 5 && food==="chum bucket"){
+       
+   }
+
+});
